4f61565e5fbd3feccdae22a9baef28af75e995c4
dhruv <dhruv@MacBook-Air.local>
2025-08-25 23:51:12 +0530

    online db

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
-const mongoURL = 'mongodb://localhost:27017/information'
+// const mongoURL = 'mongodb://localhost:27017/information'
+const mongoURL = 'mongodb+srv://admin:hellodhruv@votingapp.fle2nxm.mongodb.net/information'
 
 mongoose.connect(mongoURL,{
     
